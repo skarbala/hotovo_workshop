cf0f632dda9bf083a90a566b08661973b51520d9
martinskarbala <martin@supernova.io>
2026-07-21 12:54:37 +0200

    test added

--- tests/quoteGenerator.spec.ts
+++ tests/quoteGenerator.spec.ts
@@ -15,9 +15,24 @@ test("remove quote button is disabled on page open", async ({ page }) => {
   await expect(page.getByRole("button", { name: "Remove Quote" })).toBeDisabled();
 });
 
+test("wisdom points increase by 5 after five Get Quote clicks", async ({
+  page,
+}) => {
+  const numberOfClicks = 5;
+  await page.goto("http://localhost:8080/#/quotes");
+  const getQuoteButton = page.getByRole("button", { name: "Get Quote" });
+
+
+  for (let i = 0; i < numberOfClicks; i++) {
+    await getQuoteButton.click();
+    await expect(page.locator('p[data-test="wisdom-points"]')).toContainText((i + 1).toString());
+  }
+  await expect(page.locator('p[data-test="wisdom-points"]')).toContainText(numberOfClicks.toString());
+});
+
 // samostatná práca
-// overte ze button remove quote je po otvoreni stranky disabled
-// 5x kliknite na add quote a overte wisdom points (+5)
-// overte hlasku ktora sa zobrazi ak je list prazdny
+// 1. overte ze button remove quote je po otvoreni stranky disabled
+// 2. 5x kliknite na add quote a overte wisdom points (+5)
+// 3. overte hlasku ktora sa zobrazi ak je list prazdny
 
 //https://github.com/skarbala/hotovo_workshop
