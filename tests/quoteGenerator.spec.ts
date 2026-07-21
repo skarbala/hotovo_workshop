0b550b9d838b9f145e39583f24ca6c09b9ccab58
martinskarbala <martin@supernova.io>
2026-07-21 12:36:58 +0200

    added skills

--- tests/quoteGenerator.spec.ts
+++ tests/quoteGenerator.spec.ts
@@ -10,9 +10,14 @@ test("it displays quote on button click", async ({ page }) => {
   await expect(page.locator("ul.quote-list").locator("li")).toHaveCount(1);
 });
 
-test("test A", async ({ page }) => {});
+test("remove quote button is disabled on page open", async ({ page }) => {
+  await page.goto("http://localhost:8080/#/quotes");
+  await expect(page.getByRole("button", { name: "Remove Quote" })).toBeDisabled();
+});
 
 // samostatná práca
 // overte ze button remove quote je po otvoreni stranky disabled
 // 5x kliknite na add quote a overte wisdom points (+5)
 // overte hlasku ktora sa zobrazi ak je list prazdny
+
+//https://github.com/skarbala/hotovo_workshop
