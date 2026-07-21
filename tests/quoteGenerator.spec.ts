e3afa548639eba592655015e07161a0f6666b2b8
martinskarbala <martin@supernova.io>
2026-07-21 13:02:44 +0200

    removes comments

--- tests/quoteGenerator.spec.ts
+++ tests/quoteGenerator.spec.ts
@@ -6,10 +6,7 @@ test.describe("Quote Generator", () => {
   });
 
   test("it displays quote on button click", async ({ page }) => {
-    //1. otvorim stranku
-    //2. kliknem na button
     await page.locator('[data-test="get-quote"]').click();
-    //3. overim ze hlaska sa zobrazila
     await expect(page.locator("ul.quote-list").locator("li")).toBeVisible();
     await expect(page.locator("ul.quote-list").locator("li")).toHaveCount(1);
   });
