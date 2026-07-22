56eaf16f08afed33ccf39a9bfbbda51c4c936956
martinskarbala <martin@supernova.io>
2026-07-22 12:07:23 +0200

    adds test

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,14 +29,18 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+    video: 'retain-on-failure',
+    screenshot: 'on'
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome']
+      },
     },
 
     // {
